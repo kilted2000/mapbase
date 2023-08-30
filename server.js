5724601dda389c39ad226e20938c5c120d52dcb7
Richard <richardlwhittington@gmail.com>
2023-08-29 20:01:21 -0500

    add layers

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-//const index = require('./index.js');
 const app = express();
+const path = require('path');
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
+
+
 
 
 app.get("/", function(req, res) {
