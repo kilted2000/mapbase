eba149eb90d38346d749964ef9ec964408ce5218
Richard <richardlwhittington@gmail.com>
2023-09-09 14:05:49 -0500

    add port

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 const path = require('path');
+const PORT = process.env.PORT || 3000;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
@@ -19,5 +20,6 @@ app.get("/", function(req, res) {
 
 
 
-app.listen(3000, function() {
-});
+    app.listen(PORT, () => {
+      console.log(`server started on port ${PORT}`);
+    });
