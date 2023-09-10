5a290a92bbe9daa51fd4d9105439b445de4cfcf7
Richard <richardlwhittington@gmail.com>
2023-09-10 15:04:21 -0500

    minify

--- server.js
+++ server.js
@@ -1,12 +1,45 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
+const { minify } = require("terser");
 const PORT = process.env.PORT || 3000;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 
+
+const fs = require("fs").promises;
+
+async function minifyJavaScript() {
+  try {
+    // Read the original JavaScript file
+    const originalCode = await fs.readFile("index.js", "utf8");
+
+    // Minify the JavaScript code
+    const result = await minify(originalCode, {
+      sourceMap: {
+        // Enable source map generation
+        filename: "minified.js",
+        url: "minified.js.map",
+      },
+    });
+
+    // Write the minified code to a new file
+    await fs.writeFile("minified.js", result.code, "utf8");
+
+    // Write the source map to a new file
+    await fs.writeFile("minified.js.map", result.map, "utf8");
+
+    console.log("JavaScript minified and saved as minified.js");
+    console.log("Source map saved as minified.js.map");
+  } catch (error) {
+    console.error("Error while minifying:", error);
+  }
+}
+
+// Call the minification function
+minifyJavaScript();
 
 
 app.get("/", function(req, res) {
