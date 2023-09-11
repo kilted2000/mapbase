08609055325792277229a3c8dd304845e56fb97b
Richard <richardlwhittington@gmail.com>
2023-09-11 18:51:42 -0500

    tha

--- routeCache.js
+++ routeCache.js
@@ -1,26 +1,26 @@
-// const NodeCache = require("node-cache");
-import NodeCache from "node-cache";
-const cache = new NodeCache();
+// // const NodeCache = require("node-cache");
+// import NodeCache from "node-cache";
+// const cache = new NodeCache();
 
-const routeCache = duration => (req, res, next) => {
-    if (req.method !== "GET") {
-        console.error("Cannot cache non-GET methods!");
-        return next();
-    }
-    const key = req.originalUrl;
-    const cachedResponse = cache.get(key);
+// const routeCache = duration => (req, res, next) => {
+//     if (req.method !== "GET") {
+//         console.error("Cannot cache non-GET methods!");
+//         return next();
+//     }
+//     const key = req.originalUrl;
+//     const cachedResponse = cache.get(key);
   
-    if (cachedResponse) {
-        console.error(`Cache hit for ${key}`);
-        res.send(cachedResponse);
-      } else {
-        console.log(`Cache miss for ${key}`);
-        res.originalSend = res.send;
-        res.send = body => {
-          res.originalSend(body);
-          cache.set(key, body, duration);
-        };
-        next();
-    }
-};
-export default routeCache;
+//     if (cachedResponse) {
+//         console.error(`Cache hit for ${key}`);
+//         res.send(cachedResponse);
+//       } else {
+//         console.log(`Cache miss for ${key}`);
+//         res.originalSend = res.send;
+//         res.send = body => {
+//           res.originalSend(body);
+//           cache.set(key, body, duration);
+//         };
+//         next();
+//     }
+// };
+// export default routeCache;
